2d4dd1c7239579376dba60b0976b7a24d606c85e
Dagmawi Mulualem <Dagmawi.m.mulualem@gmail.com>
2025-07-30 14:31:48 -0400

    Update: modified homepage and reservation logic

--- server.js
+++ server.js
@@ -140,9 +140,8 @@ app.post('/contact', async (req, res) => {
 
 // ---------- 404
 app.use((req, res) => {
-  res.status(404).send('404 - Page Not Found');
+  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
-
 
 // ---------- Start
 app.listen(PORT, () => {
